c5add532abcbc8bcb13d37d83698248cb436062b
Minator-web <63640410+Minator-web@users.noreply.github.com>
2022-11-16 18:49:57 +0330

    Green 2

--- test.py
+++ test.py
@@ -6,10 +6,12 @@ class  Fizzbuzz(unittest.TestCase):
     def  test_1(self):
         self.assertEqual(FizzBuzz(3),"buzz")
     def test_a(self):
-        self.assertEqual(fizzBuzz(5),"fizz")
+        self.assertEqual(FizzBuzz(5),"fizz")
 
 
 def FizzBuzz(a):
     if(a == 3):
         return "buzz"
+    elif(a%5 == 0):
+        return "fizz"
 unittest.main()

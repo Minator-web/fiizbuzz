bb18a44492a879eca21a042c0cf81580066018cf
Minator-web <63640410+Minator-web@users.noreply.github.com>
2022-11-16 18:46:14 +0330

    Green 1

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import unittest
 
 class  Fizzbuzz(unittest.TestCase):
-    def test_(self):
+    def test_1(self):
         Fizzbuzz(1)
-    
+    def  test_1(self):
+        self.assertEqual(FizzBuzz(3),"buzz")
+
+
+def FizzBuzz(a):
+    if(a == 3):
+        return "buzz"
 unittest.main()

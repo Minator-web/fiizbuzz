f3d0f8faef1116806f0225b6b4a33c7ea73816e6
Minator-web <63640410+Minator-web@users.noreply.github.com>
2022-11-16 18:55:47 +0330

    Green 3

--- test.py
+++ test.py
@@ -12,8 +12,11 @@ class  Fizzbuzz(unittest.TestCase):
 
 
 def FizzBuzz(a):
+    if(a%5 == 0 and a%3 == 0):
+        return "fizzbuzz"
     if(a == 3):
         return "buzz"
     elif(a%5 == 0):
         return "fizz"
+   
 unittest.main()

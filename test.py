a8025bbd557457b57447cf89576102fd7738e1c4
Minator-web <63640410+Minator-web@users.noreply.github.com>
2022-11-16 18:48:55 +0330

    Red 2

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ class  Fizzbuzz(unittest.TestCase):
         Fizzbuzz(1)
     def  test_1(self):
         self.assertEqual(FizzBuzz(3),"buzz")
+    def test_a(self):
+        self.assertEqual(fizzBuzz(5),"fizz")
 
 
 def FizzBuzz(a):
